95eb645c31b8ed2315cd08d7f5e713d0b0c3eb29
Anushka Jana <your-email@example.com>
2026-02-11 21:20:32 +0530

    Add Netlify contact form

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,65 @@
 import React from 'react';
 import './Footer.css';
 import footer_logo from '../../assets/footer_logo.svg';
-import user_icon from '../../assets/user_icon.svg'; // ✅ import added
+import user_icon from '../../assets/user_icon.svg';
 
 const Footer = () => {
   return (
     <div className='footer'>
+
+      {/* ===== TOP SECTION ===== */}
       <div className='footer-top'>
         <div className='footer-top-left'>
           <img src={footer_logo} alt="Footer Logo" />
           <p>I am a developer</p>
         </div>
-        <div className='footer-top-right'>
+
+        {/* ✅ NETLIFY FORM START */}
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className='footer-top-right'
+        >
+          {/* Netlify hidden input */}
+          <input type="hidden" name="form-name" value="contact" />
+
           <div className='footer-email-input'>
             <img src={user_icon} alt="User Icon" />
-            <input type="email" placeholder='Enter your email' />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              required
+            />
           </div>
-          <button className='footer-subscribe'>Subscribe</button>
-        </div>
+
+          <button type="submit" className='footer-subscribe'>
+            Subscribe
+          </button>
+        </form>
+        {/* NETLIFY FORM END */}
+
       </div>
+
       <hr />
+
+      {/* BOTTOM SECTION */}
       <div className='footer-bottom'>
         <p className='footer-bottom-left'>@ 2025 Anushka Jana</p>
-        <div className='fotter-bottom-right'>
-            <p>Term of Serice</p>
-            <p>Privacy Policy</p>
-            <p>Connect with me</p>
+
+        <div className='footer-bottom-right'>
+          <p>Term of Service</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
         </div>
       </div>
+
     </div>
   );
 };
 
 export default Footer;
+
 
 
